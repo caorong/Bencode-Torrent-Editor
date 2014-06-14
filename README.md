9c58101809db147e2c6a0ea4292867746010030b
caorong <lelouchcr@gmail.com>
2014-06-14 13:10:20 +0800

    first commit, add dir support

---
+++ README.md
@@ -0,0 +1,15 @@
+# what is it 
+
+给bt种子加tracker server，提高发现做种人的可能性，提高下载速度。
+
+# how to use
+
+添加服务列表到一个种子
+``` shell
+python add_tracker.py trackersFile.txt xxx.torrent
+```
+
+添加到文件夹'test'内的所有种子
+``` shell
+python add_tracker.py trackersFile.txt test
+```
---
+++ add_tracker_dir.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python2
+
+"""
+Author: caorong  
+
+"""
+
+import bencode
+import sys
+from os import path
+import os
+
+if len(sys.argv) < 2:
+    print "First argument: File containing trackers"
+    print "Second argument: dir name contains .torrent file "
+    sys.exit(0)
+
+
+
+for torrent_file_name in os.listdir(sys.argv[2]):
+	# torrent_file = open(sys.argv[2])
+	torrent_file = open(sys.argv[2]+'/'+torrent_file_name)
+	tracker_file = open(sys.argv[1])
+
+	trackers_list = []
+	trackers_list[:] = (value for value in tracker_file.read().split("\n") if value != '')
+	decoded_data = bencode.bdecode(torrent_file.read())
+
+	print "Trackers Before: "
+	for tracker in decoded_data['announce-list']:
+	    print tracker
+
+	for tracker in trackers_list:
+	    if tracker not in decoded_data['announce-list']:
+	        decoded_data['announce-list'].append([tracker])
+
+	print "Now, the trackers in the torrent are: "
+	for tracker in decoded_data['announce-list']:
+	    print tracker
+
+	#Writing the torrent file
+	# f = open("new_"+path.basename(sys.argv[2]), "w")
+	f = open("new_"+path.basename(torrent_file_name), "w")
+	f.write(bencode.bencode(decoded_data))
+	f.close()
+
